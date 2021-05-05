938b6c8b044476830c5fa70ad04d53cbad210e55
Pedro C. Da S. Brum <63755429+pedrocdasbrum@users.noreply.github.com>
2021-05-05 14:38:16 -0300

    Add files via upload

---
+++ Atividade_Avaliativa_02/app/src/main/java/com/pedrocaetano/atividade_avaliativa_02/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pedrocaetano.atividade_avaliativa_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
